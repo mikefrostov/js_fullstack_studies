0107bc759ca0101e84f8bdf380f4a9d5f309fb68
mikefrostov <m.e.morozov1@gmail.com>
2020-07-05 14:42:10 -0400

    add oauth lib

--- nodewithreact/backend/index.js
+++ nodewithreact/backend/index.js
@@ -1,7 +1,6 @@
 const express = require ('express');
-
-
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
 const app = express();
 
@@ -9,9 +8,11 @@ app.get('/', (req, res) => {
   res.send({ h1: 'dummy' });
 });
 
+passport.use(new GoogleStrategy());
+
+
 //getting port from environment variable PORT or 5001
 const PORT = process.env.PORT || 5001;
-
 
 app.listen(PORT , function(){
     console.log('Server running on 5001...');
